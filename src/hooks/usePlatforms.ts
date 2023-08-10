a74690b75e8e1ca6694d64b9607786e632f48a74
refat <khondakar00@gmail.com>
2023-08-10 02:27:55 -0700

    Fixing data error

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -15,7 +15,7 @@ const usePlatforms = () =>
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24h,
-    initialData: { count: platforms.length, results: platforms }
+    // initialData: { count: platforms.length, results: platforms }
   });
 
 export default usePlatforms;
