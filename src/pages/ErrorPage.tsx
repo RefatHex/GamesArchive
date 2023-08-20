0bc56134819df107ba75d2597e3ae785ab38eb6a
refat <khondakar00@gmail.com>
2023-08-20 07:39:33 -0700

    Handle routes error

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,22 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <>
+      <NavBar />
+      <Box padding={5}>
+        <Heading>Oops</Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "This page doesn't exist."
+            : "An unexpected error occured"}
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default ErrorPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 import GameDetailPage from "./pages/GameDetailPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
@@ -6,6 +7,7 @@ import Layout from "./pages/Layout";
 const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <ErrorPage />,
     element: <Layout />,
     children: [
       { index: true, element: <HomePage /> },
