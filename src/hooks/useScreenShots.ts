09dacefb92e19acf8b4f40c86c965bdac0f19bf3
refat <khondakar00@gmail.com>
2023-08-20 13:56:57 -0700

    build game screeenshot

---
+++ src/hooks/useScreenShots.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { Screenshot } from "../entities/screenshot";
+import APIClient from "../services/api-client";
+
+const useScreenshots=(gameId:number)=>{
+    const apiCLient=new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
+
+    return useQuery({
+        queryKey:['screenshots', gameId],
+        queryFn:apiCLient.getAll
+    })
+}
+
+export default useScreenshots;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,6 +2,7 @@ import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameScreenshots from "../components/GameScreenshots";
 import GameTrailer from "../components/GameTrailer";
 import useGame from "../hooks/useGameDetail";
 
@@ -18,6 +19,7 @@ const GameDetailPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
